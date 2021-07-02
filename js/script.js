a26179e11ba3bd9408a8203a39a7cfd4b7676630
Sergey Kulynets <kylun1serg@gmail.com>
2021-07-02 10:49:57 +0300

    practice_one

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-'use strict';
 
-const category = 'toys';
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-console.log (`https://site.ru/${category}`);
-
-const user = 'Ivan';
+console.log(numberOfFilms);
 
 
-alert(`Hello ${user}!`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const rating = prompt('На сколько оцените его?', '');
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?', '');
+const rating1 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies.lastFilm = rating;
+personalMovieDB.movies[lastFilm1] = rating1;
+
+
+
+const mov = {
+    [lastFilm]: rating,
+};
+
+console.log(personalMovieDB);
+console.log(mov);
